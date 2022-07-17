b5c45ec9d48ac990a95bb3529a6e0a267019a486
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-07-17 14:38:20 +0300

    commit tree 81357b7317d84dfeb2cca14794259ce7a0075b59

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
-  function handleAnswer(isCorrect) {
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; 
+    }
+
+    //  a timeout to run after 1 second
+    const timerId = setTimeout(() => {
+      // decreasing the time remaining
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // a clean up function to clean up after the timeout function
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+
+  function handleCorrectAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
@@ -19,7 +39,7 @@ function Question({ question, onAnswered }) {
       {answers.map((answer, index) => {
         const isCorrect = index === correctIndex;
         return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+          <button key={answer} onClick={() => handleCorrectAnswer(isCorrect)}>
             {answer}
           </button>
         );
